866754ed17df2087fba3c88540bde16cd30ec6a1
khurram676 <khurramshabir475@gmail.com>
2024-12-22 21:55:56 -0800

    turtle in python

---
+++ main.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.Screen().bgcolor("green")
+sc=turtle.Screen()
+sc.setup(100,100)
+
+turtle.title("rectangle")
+boared=turtle.Turtle()
+boared.fillcolor("black")
+boared.forward(100)
+boared.left(90)
+
+boared.forward(100)
+boared.left(90)
+
+boared.forward(100)
+boared.left(90)
+
+boared.forward(100)
+boared.left(90)
+boared.end_fill()
+turtle.done()
